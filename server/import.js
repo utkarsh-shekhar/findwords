802211912ce1be59eaa42c9448264a59cc164bd0
Kranthi <kranthi117@gmail.com>
2018-03-17 17:24:15 +0530

    server functions

--- server/import.js
+++ server/import.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
-const utils = require("./utils")
+const utils = require("./utils");
 
 var dbURI = 'mongodb://localhost/findwords'; 
 var connection = mongoose.connect(dbURI);
 
 const onConnection = async function () {  
 	const grid = mongoose.model('Grid', { grid: Object, word_list: Object, count: String });
-	const g = new grid(utils.generateGrid());
-	await g.save();
+	for (let i = 0; i < 10000; i++)  {
+		const g = new grid(utils.generateGrid());
+		await g.save();
+	}
 	mongoose.connection.close();
 }
 mongoose.connection.on('connected', onConnection); 
--- server/index.js
+++ server/index.js
@@ -2,22 +2,74 @@ var app = require("express")();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var path = require('path');
+const mongoose = require('mongoose');
+const utils = require("./utils");
+
+var dbURI = 'mongodb://localhost/findwords'; 
+var connection = mongoose.connect(dbURI);
+const grid = mongoose.model('Grid', { grid: Object, word_list: Object, count: String });
+var game = undefined;
 
 app.get("/", function(req, res){
   res.sendFile(path.resolve(__dirname+"/../client/index.html"));
 });
 
+http.listen(3000, function(){
+  console.log("listening on *:3000");
+});
+
+const sendLeaderboard = function() {
+  if (game) {
+    io.emit("leaderboard", game["leaderboard"])
+  }
+}
+
+const calculateScore = async function(msg) {
+  const wordList = game["word_list"];
+  const word = msg["word"];
+  const userId = parseInt(msg["_id"], 16) % 100000;
+  let newScore;
+  let alreadyFound = false;
+  if (words[userId].includes(word)) {
+    newScore = 0;
+    alreadyFound = false;
+  }
+  newScore = utils.getScore(wordList, word);
+  const score = game["leaderboard"][userId] + newScore;
+  game["leaderboard"][userId] = score;
+  socket.emit("socre", {"user_id": userId, "score": score, "valid": newScore > 0, "already_found": alreadyFound})
+}
+
+const stopGame = async function() {
+  console.log("stoping game")
+  io.emit("game ended")
+  game = undefined;
+  setTimeout(startGame, 1000)
+}
+
+const startGame = async function() {
+  grid.findOne().then(function(result) {
+    newGame = game = result;
+    game["leaderboard"] = {}
+    newGame["word_list"] = undefined;
+    io.emit("game started", newGame);
+    setTimeout(stopGame, 1000);
+  })
+}
+
+setInterval(sendLeaderboard, 1000);
+
 io.on("connection", function(socket){
   console.log("a user connected");
-  socket.on("chat message", function(msg){
-    console.log("message: " + msg);
-    io.emit("chat message", msg);
-  });
+
+  if(game)  {
+    socket.emit("game started", game);
+  }
+
+  socket.on("calculate score", calculateScore);
   socket.on("disconnect", function(){
     console.log("user disconnected");
   });
 });
 
-http.listen(3000, function(){
-  console.log("listening on *:3000");
-});
+startGame();
