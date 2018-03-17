755de3702baff0978a2fd4055a2ae0cb26aed35e
Kranthi <kranthi117@gmail.com>
2018-03-17 15:10:04 +0530

    util functions

---
+++ server/utils.js
@@ -0,0 +1,16 @@
+function getGrids() {
+  return {
+    "grid": [
+      [{"character": "A", "score": 1},{"character": "A", "score": 1},{"character": "A", "score": 1},{"character": "A", "score": 1}],
+      [{"character": "A", "score": 1},{"character": "A", "score": 1},{"character": "A", "score": 1},{"character": "A", "score": 1}],
+      [{"character": "A", "score": 1},{"character": "A", "score": 1},{"character": "A", "score": 1},{"character": "A", "score": 1}],
+      [{"character": "A", "score": 1},{"character": "A", "score": 1},{"character": "A", "score": 1},{"character": "A", "score": 1}]
+    ],
+    "count": 10,
+    "word_list": ["ABC"]
+  };
+}
+
+function getScoure(list, word) {
+  return 10;
+}
